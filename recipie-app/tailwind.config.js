5f6b647f0583aa2e0e76219ef0a2a27e1056ebdb
rohan <gargrohan138@gmail.com>
2024-09-13 22:56:45 +0530

    adding and learning react router dom

---
+++ recipie-app/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/details/index.jsx",
+    "./src/pages/favourites/index.jsx",
+    "./src/pages/home/index.jsx",
+    "./src/components/navbar/index.jsx",
+    "./src/App.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
