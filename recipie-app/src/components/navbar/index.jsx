dd17983a40e05989b0253ebd18ebbeb4efe66108
rohan <gargrohan138@gmail.com>
2024-09-15 22:54:44 +0530

    making navbar component

--- recipie-app/src/components/navbar/index.jsx
+++ recipie-app/src/components/navbar/index.jsx
@@ -1,3 +1,15 @@
+import '../../output.css'
+
 export default function Navbar(){
-    return <div>Navbar</div>
+    return <nav className="flex justify-between items-center py-8 container mx-auto flex-col lg:flex-row gap-5 lg:gap:0">
+        <h2 className="text-2xl font-semibold">FoodRecipie</h2>
+        <forms>
+            <input
+            type='text'
+            name='search'
+            placeholder='Enter Items...'
+            className='bg-black/75 p-3 px-8 rounded-full outline-none lg:w-96 shadow-lg shadow-red-300 focus:shadow-red-400'
+            />
+        </forms>
+    </nav>
 }
