b50840f5c5d4a69fefd61c9f659599e4efa90e17
rohan <gargrohan138@gmail.com>
2024-09-16 23:12:37 +0530

    navbar completed

--- recipie-app/src/components/navbar/index.jsx
+++ recipie-app/src/components/navbar/index.jsx
@@ -1,8 +1,9 @@
+import { NavLink } from 'react-router-dom'
 import '../../output.css'
 
 export default function Navbar(){
     return <nav className="flex justify-between items-center py-8 container mx-auto flex-col lg:flex-row gap-5 lg:gap:0">
-        <h2 className="text-2xl font-semibold">FoodRecipie</h2>
+        <h2 className="text-2xl font-semibold"><NavLink to={"/"}>FoodRecipie</NavLink></h2>
         <forms>
             <input
             type='text'
@@ -11,5 +12,13 @@ export default function Navbar(){
             className='bg-black/75 p-3 px-8 rounded-full outline-none lg:w-96 shadow-lg shadow-red-300 focus:shadow-red-400'
             />
         </forms>
+        <ul className='flex gap-5'>
+            <li>
+                <NavLink to={"/"} className="text-black hover:text-gray-700 duration-300">Home</NavLink>
+            </li>
+            <li>
+                <NavLink to={"/favourites"} className="text-black hover:text-gray-700 duration-300">Favourites</NavLink>
+            </li>
+        </ul>
     </nav>
 }
