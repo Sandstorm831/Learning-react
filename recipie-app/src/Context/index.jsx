7e3b7d3016dd6cbe658ee422b9d7530f07b904d4
rohan <gargrohan138@gmail.com>
2024-09-25 23:02:52 +0530

    adding details page

--- recipie-app/src/Context/index.jsx
+++ recipie-app/src/Context/index.jsx
@@ -2,27 +2,28 @@ import { createContext, useState } from "react";
 
 export const GlobalContext = createContext(null);
 
-export default function GlobalState({children}){
+export default function GlobalState({ children }) {
 
     const [searchParam, setSearchParam] = useState('')
     const [loading, setLoading] = useState(false)
     const [recipeList, setRecipeList] = useState([])
+    const [recipeDetailsData, setRecipeDetailsData] = useState(null)
 
-    async function handleSubmit(event){
+    async function handleSubmit(event) {
         event.preventDefault()
         console.log("here")
-        try{
+        try {
             const response = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}`);
             const data = await response.json()
             console.log(data)
-            if(data?.data?.recipes){
+            if (data?.data?.recipes) {
                 setRecipeList(data?.data?.recipes)
                 setLoading(false)
                 setSearchParam('')
             }
 
 
-        } catch(error){
+        } catch (error) {
             console.log(error)
             setLoading(false)
             setSearchParam('')
@@ -32,5 +33,5 @@ export default function GlobalState({children}){
     console.log(loading, recipeList)
 
 
-    return <GlobalContext.Provider value={{searchParam, setSearchParam, handleSubmit, loading, recipeList}}> { children } </GlobalContext.Provider>;
+    return <GlobalContext.Provider value={{ searchParam, setSearchParam, handleSubmit, loading, recipeList, recipeDetailsData, setRecipeDetailsData }}> {children} </GlobalContext.Provider>;
 }
--- recipie-app/src/pages/details/details.jsx
+++ recipie-app/src/pages/details/details.jsx
@@ -1,3 +1,23 @@
+import { useContext, useEffect } from "react"
+import { useParams } from "react-router-dom"
+import { GlobalContext } from "../../Context"
+
 export default function Details(){
+
+
+    const {recipeDetailsData, setRecipeDetailsData} = useContext(GlobalContext)
+    const {id} = useParams()
+
+
+    useEffect(() => {
+        async function getRecipeDetails() {
+            const response = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`)
+            const data = await response.json()
+            console.log(data)
+        }
+        getRecipeDetails()
+
+    }, [id])
+
     return <div>Details</div>
 }
