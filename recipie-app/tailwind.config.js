2a90f827aef54fb48eaece6452bb0b19c34859aa
rohan <gargrohan138@gmail.com>
2024-10-02 22:47:34 +0530

    final touches

--- recipie-app/tailwind.config.js
+++ recipie-app/tailwind.config.js
@@ -5,7 +5,9 @@ module.exports = {
     "./src/pages/favourites/index.jsx",
     "./src/pages/home/index.jsx",
     "./src/components/navbar/index.jsx",
-    "./src/App.js"
+    "./src/App.js",
+    "./src/components/recipe_item/index.jsx",
+    "./src/pages/details/details.jsx"
   ],
   theme: {
     extend: {},
